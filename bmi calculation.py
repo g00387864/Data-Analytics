e57e7d17c50e1528a04be31a8f55769985163a38
G00387864 <G00387864@GMIT.ie>
2020-01-24 15:05:59 +0000

    adding bmi calculator

---
+++ bmi calculation.py
@@ -0,0 +1,11 @@
+#calculating BMI for week 2 exercise
+#metric BMI formula = Weight (KG) ÷ Height (Metres²)
+
+weight = float(input("enter weight in kg: "))
+height = float(input("enter height in metres: "))
+
+bmi = (weight / (height*hight))
+
+print (bmi)
+
+
